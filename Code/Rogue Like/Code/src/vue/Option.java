57a9fc28ba4be8e1d3eda39c86747783433d5dde
Bauer Victor <victor.bauer@etu.unistra.fr>
2014-11-18 09:26:57 +0100

    option pas beau
    option

--- Code/Rogue Like/Code/src/vue/Option.java
+++ Code/Rogue Like/Code/src/vue/Option.java
@@ -21,10 +21,11 @@ public class Option extends JFrame {
     public Option(){
 
 
-        JFrame jfOption = new JFrame();
-        JPanel jpOption = new JPanel(new GridLayout(5,5));
+        final JFrame jfOption = new JFrame();
+        JPanel jpOption = new JPanel(new GridLayout(6,5));
         this.setContentPane(jpOption);
         JButton jbOk=new JButton();
+        jbOk.setText("Ok");
        
 
         JLabel lblProfMax = new JLabel("La profondeur maximum de votre souterrain : ");
@@ -32,11 +33,11 @@ public class Option extends JFrame {
         JLabel lblProbMonstre = new JLabel("La probabilité de générer un monstre : ");
         JLabel lblProbTresor = new JLabel("La probabilité de générer un trésor : ");
         JLabel lblProbPotion = new JLabel("La probabilité de générer une potion : ");
-        JTextField tfProfMax = new JTextField(5);
-        JTextField tfProbEsca = new JTextField(5);
-        JTextField tfProbMonstre = new JTextField(5);
-        JTextField tfProbTresor = new JTextField(5);
-        JTextField tfProbPotion = new JTextField(5);
+        final JTextField tfProfMax = new JTextField(5);
+        final JTextField tfProbEsca = new JTextField(5);
+        final JTextField tfProbMonstre = new JTextField(5);
+        final JTextField tfProbTresor = new JTextField(5);
+        final JTextField tfProbPotion = new JTextField(5);
 
 
         jpOption.add(lblProfMax);
@@ -49,16 +50,37 @@ public class Option extends JFrame {
         jpOption.add(tfProbTresor);
         jpOption.add(lblProbPotion);
         jpOption.add(tfProbPotion);
+        jpOption.add(jbOk);
 
         this.setTitle("Réglage de lancement du Rogue Like");
         this.setSize(600, 500);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       
         
         jbOk.addActionListener(new ActionListener() {
                  public void actionPerformed(ActionEvent e) {
-                    
+                     if(tfProfMax.getText().length()==0 || tfProbEsca.getText().length()==0 || tfProbMonstre.getText().length()==0 ||tfProbTresor.getText().length()==0 ||tfProbPotion.getText().length()==0 )
+                     {
+                         JOptionPane alert = new JOptionPane();
+                         alert.showMessageDialog(null, " Attention champs vide ! "," Erreur ! ",
+                         JOptionPane.ERROR_MESSAGE);
+                        
+                     }
+                     else{  
+                             System.out.println(tfProfMax.getText());
+                             System.out.println(tfProbEsca.getText());
+                             System.out.println(tfProbMonstre.getText());
+                             System.out.println(tfProbTresor.getText());
+                             System.out.println(tfProbPotion.getText());
+                             System.exit(0);
+                         }
+                     
                  }
               });
 
+    }
+   public static void main(String[] args){
+        Option op = new Option();
+        op.setVisible(true);
     }
 }
